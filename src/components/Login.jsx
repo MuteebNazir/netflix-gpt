ff39f5ca73027f3c3ee4ca05ca937d72572d9747
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-02 00:15:07 +0530

    feat: Add z-index to Header component and restructure Login component layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,22 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/cc73e7c7-7860-4ef4-8fc8-1baf24569d2f/web/IN-en-20260126-TRIFECTA-perspective_90d714e8-acc9-4253-ab46-ca6b349c1989_large.jpg" alt="Background" />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cc73e7c7-7860-4ef4-8fc8-1baf24569d2f/web/IN-en-20260126-TRIFECTA-perspective_90d714e8-acc9-4253-ab46-ca6b349c1989_large.jpg"
+          alt="Background"
+        />
+      </div>
+      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
+        <h1 className="font-bold text-2xl py-4"> Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full"
+        />
+        <input type="text" placeholder="Password" className="p-2 my-2 w-full" />
+        <button className="p-4 my-2 bg-red-700  w-full">Sign in</button>
+      </form>
     </div>
   );
 };

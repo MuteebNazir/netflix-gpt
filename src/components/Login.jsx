60ef268f25649e2666a4293d19eb5f6b2249a0d7
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-02 13:57:20 +0530

    refactor: Improve code formatting and readability in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,23 +18,34 @@ const Login = () => {
         />
       </div>
       <form className="absolute w-4/12 p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-2xl text-white">
-        <h1 className="font-bold text-2xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        <h1 className="font-bold text-2xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+         {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-700 rounded-lg"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
           className="p-2 my-4 w-full bg-gray-700 rounded-lg"
         />
+       
         <input
           type="text"
           placeholder="Password"
           className="p-2 my-2 w-full bg-gray-700 rounded-lg"
         />
         <button className="p-4 my-4 bg-red-700  w-full rounded-lg ">
-         {isSignInForm ? "Sign In" : "Sign Up"}
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4" onClick={toggleSignInForm}>
-          {" "}
-          New to Netflix? Sign Up Now{" "}
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now."
+            : "Already registered? Sign In Now."}{" "}
         </p>
       </form>
     </div>

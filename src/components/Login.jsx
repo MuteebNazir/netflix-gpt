d7e82809dacfa0b3b1916ee2be31d24c3abc87aa
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-02 13:47:12 +0530

    feat: Implement toggle functionality for Sign In/Sign Up forms in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -12,15 +18,24 @@ const Login = () => {
         />
       </div>
       <form className="absolute w-4/12 p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-2xl text-white">
-        <h1 className="font-bold text-2xl py-4"> Sign In</h1>
+        <h1 className="font-bold text-2xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         <input
           type="text"
           placeholder="Email Address"
           className="p-2 my-4 w-full bg-gray-700 rounded-lg"
         />
-        <input type="text" placeholder="Password" className="p-2 my-2 w-full bg-gray-700 rounded-lg" />
-        <button className="p-4 my-4 bg-red-700  w-full rounded-lg ">Sign in</button>
-      <p className="py-4"> New to Netflix? Sign Up Now </p>
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-4 bg-red-700  w-full rounded-lg ">
+         {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4" onClick={toggleSignInForm}>
+          {" "}
+          New to Netflix? Sign Up Now{" "}
+        </p>
       </form>
     </div>
   );

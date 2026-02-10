81290a8a0c689bdcfe9d99c73c87e081b3b87628
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-10 23:45:51 +0530

    feat: restore useNowPlayingMovies hook for fetching now playing movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,8 +1,8 @@
-import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
 
 const Browse = () => {
-  
+
   useNowPlayingMovies();
   return (
     <div>
--- src/hooks/useNowPlayingMovies.jsx
+++ src/hooks/useNowPlayingMovies.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { API_OPTIONS } from "../src/utils/constants";
-import { addNowPlayingMovies } from "../src/utils/moviesSlice";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const useNowPlayingMovies = () => {
   //* Fetch data from TMDB API and update the Store

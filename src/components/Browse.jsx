4bd3e6e6170cd720624482e9da784a38c7671f7f
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-12 14:46:32 +0530

    feat: add MainContainer, SecoundaryContainer, VideoBackground, and VideoTitle components to enhance Browse layout

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,5 +1,7 @@
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecoundaryContainer from "./SecoundaryContainer";
 
 const Browse = () => {
 
@@ -7,6 +9,16 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecoundaryContainer />
+      {/*
+      MainContainer 
+      - VideoBackground
+      - VideoTitle
+      - SecoundaryContainer 
+      -MovieList * n
+      - Cards * n
+      */}
     </div>
   );
 };
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
+
+const MainContainer = () => {
+  const movies = useSelector((state) => state.movies?.nowPlayingMovies);
+
+  if (!movies) return;
+  
+  const mainMovie = movies[0];
+  console.log(mainMovie);
+  
+  return (
+    <div>
+      <VideoTitle />
+      <VideoBackground />
+    </div>
+  );
+};
+
+export default MainContainer;

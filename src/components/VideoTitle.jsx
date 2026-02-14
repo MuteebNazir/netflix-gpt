b07012f6c3274d9c85b91a805df72a110525109e
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-14 13:43:04 +0530

    fix: update VideoBackground iframe src for autoplay and loop functionality; enhance VideoTitle button hover effect

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -6,7 +6,7 @@ const VideoTitle = ({ title, overview }) => {
     <p className="text-lg mt-2 ml-10 w-1/3 ">{overview}</p>
      <div className="ml-10 mt-4">
 
-      <button className="bg-white text-black p-4 px-12 rounded-md font-bold">Play</button>
+      <button className="bg-white text-black p-4 px-12 rounded-md font-bold hover:bg-opacity-80">Play</button>
       <button className="bg-gray-500 text-white p-4 px-12 rounded-md font-bold ml-4 hover:bg-opacity-80"> More Info</button>
     </div>
     </div>

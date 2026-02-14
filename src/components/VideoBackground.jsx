1590054ebc426ba9abf94f92d503ebbd08bfbc67
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-14 12:30:38 +0530

    feat: create useMovieTrailer hook for fetching and managing trailer video data

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,33 +1,10 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { API_OPTIONS } from "../utils/constants";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
- 
-const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-const dispatch = useDispatch();
-  
-//* Fetch trailer Video and updating the store with trailer video data.
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/840464/videos?language=en-US",
-      API_OPTIONS,
-    );
-    const json = await data.json();
-    console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-
+  useMovieTrailer(movieId);
   return (
     <div>
       <iframe

89a29e6dc9a5317a79ae6b8098498f471e2f217a
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-15 23:48:05 +0530

    fix: correct formatting in MovieCard and MovieList components; ensure proper console logging and consistent IMG_CDN_URL declaration

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,14 @@
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  console.log(movies)
+  console.log(movies);
     return (
     <div>
       <div>
         <h1>{title}</h1>
         <div>
-          <MovieCard  />
+          {movies.map((movie) => <MovieCard key={movie.id} posterPath={movie.poster_path} />)}
+          
         </div>
       </div> 
     </div>

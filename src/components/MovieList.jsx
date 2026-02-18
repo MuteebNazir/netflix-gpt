e99a18fbf87fdf0d9b43552f4cacbb91def0b097
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-18 12:49:54 +0530

    Implemented Scrollbar movement smooth for better UX.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,13 @@ const MovieList = ({ title, movies }) => {
        <h1 className="text-pretty text-xl font-semibold py-2 text-white">{title}</h1>
    <div
   className="flex overflow-x-auto overflow-y-hidden scroll-smooth cursor-grab active:cursor-grabbing no-scrollbar"
-  onWheel={(e) => (e.currentTarget.scrollLeft += e.deltaY * 0.7)}
+onWheel={(e) => {
+  e.preventDefault();
+  const el = e.currentTarget;
+
+  el.scrollLeft += e.deltaY * 0.5;
+  el.scrollLeft += e.deltaX * 1.5;
+}}
 >
         <div className="flex ">
           {movies?.map((movie) => <MovieCard key={movie.id} posterPath={movie.poster_path} />)}

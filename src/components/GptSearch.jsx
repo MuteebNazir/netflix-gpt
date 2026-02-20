985c04480d4190dddd9b7fdaa727462f271a1408
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-20 16:22:08 +0530

    Implemented Tailwind styles on searchbar and included background image.

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -4,6 +4,12 @@ import GptMovieSuggestions from "./GptMovieSuggestions";
 
 const GptSearch = () => {
   return <div>
+    <div className="absolute -z-10">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cc73e7c7-7860-4ef4-8fc8-1baf24569d2f/web/IN-en-20260126-TRIFECTA-perspective_90d714e8-acc9-4253-ab46-ca6b349c1989_large.jpg"
+          alt="Background"
+        />
+      </div>
     <GptSearchBar/>
     <GptMovieSuggestions/>
   </div>;

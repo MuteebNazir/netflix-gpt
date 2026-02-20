6ea5353f443cb91e0a6a07119d711a5217662bc0
Muteeb Nazir <99727002+MuteebNazir@users.noreply.github.com>
2026-02-20 16:29:04 +0530

    Added BG URL in constant to avoid repeat coding.

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,12 +1,15 @@
+
+
 import React from "react";
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
+import { BG_URL } from "../utils/constants";
 
 const GptSearch = () => {
   return <div>
     <div className="absolute -z-10">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/cc73e7c7-7860-4ef4-8fc8-1baf24569d2f/web/IN-en-20260126-TRIFECTA-perspective_90d714e8-acc9-4253-ab46-ca6b349c1989_large.jpg"
+          src={BG_URL}
           alt="Background"
         />
       </div>
